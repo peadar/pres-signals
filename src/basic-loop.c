390372e741f35ab9f582636d7f124c9e461425fc
Peter Edwards <peadar@isainmdom.net>
2020-07-18 17:58:13 +0100

    Snap

--- src/basic-loop.c
+++ src/basic-loop.c
@@ -4,11 +4,9 @@ static void signal_handler(int signo) {
    printf("received signal %d ('%s'), current errno: %s\n", signo, strsignal(signo), strerror(errno));
    done = 1;
 }
-
 int main() {
    signal(SIGALRM, signal_handler);
    alarm(1);
-   while (!done) {
+   while (!done)
       strlen("hello world");
-   }
 }
